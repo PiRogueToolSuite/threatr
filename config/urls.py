735691ec1c2d5b8034e397f12a67de14be6ddc16
U039b <git@0x39b.fr>
2023-04-14 21:41:22 +0200

    Fix flake8

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
